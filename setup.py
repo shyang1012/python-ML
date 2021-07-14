11adc5e7822f13391160e33de0dddec1e0d72c40
shyang1012 <disroway@chungbuk.ac.kr>
2021-07-14 12:09:55 +0900

    setup.py 추가, 파일명 오류 수정, gitignore 목록 추가

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PYTHON-ML",
+    version="0.0.1",
+    author="shyang",
+    author_email="disroway@chungbuk.ac.kr",
+    description="머신러닝교과서 with 파이썬",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shyang1012/python-ML",
+    project_urls={
+        "Bug Tracker": "https://github.com/shyang1012/python-ML/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+)
