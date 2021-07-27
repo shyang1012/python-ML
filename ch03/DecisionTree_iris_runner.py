250698d011136a150321858597f9ec39a778aab7
shyang1012 <disroway@chungbuk.ac.kr>
2021-07-27 12:30:43 +0900

    post pruning 방법 적용(C4.5), 적정 ccp_alpha값 찾기위한 실험(주석)

--- ch03/DecisionTree_iris_runner.py
+++ ch03/DecisionTree_iris_runner.py
@@ -53,9 +53,66 @@ if __name__ == '__main__':
     # print("X_train_std", X_train_std)
     # print("X_test_std", X_test_std)
 
-    tree_model = DecisionTreeClassifier(criterion='gini', max_depth=4)
-    tree_model.fit(X_train_std, y_train)
+    tree_model = DecisionTreeClassifier(criterion='gini', ccp_alpha=0.05, random_state=0)
+    # tree_model = DecisionTreeClassifier(random_state=0)
+    path = tree_model.cost_complexity_pruning_path(X_train, y_train)
+    ccp_alphas, impurities = path.ccp_alphas, path.impurities
+   
+    #  적정 ccp_alpha 값을 찾기 위한 소스 시작
+    # fig, ax = plt.subplots()
+    # ax.plot(ccp_alphas[:-1], impurities[:-1], marker='o', drawstyle="steps-post")
+    # ax.set_xlabel("effective alpha")
+    # ax.set_ylabel("total impurity of leaves")
+    # ax.set_title("Total Impurity vs effective alpha for training set")
+    # plt.savefig(get_base_dir('images')+'/total impurity of leaves.png', dpi = 300)
+    # # plt.show()
+    # plt.close()
 
+    # clfs = []
+    # for ccp_alpha in ccp_alphas:
+    #     clf = DecisionTreeClassifier(random_state=0, ccp_alpha=ccp_alpha)
+    #     clf.fit(X_train, y_train)
+    #     clfs.append(clf)
+    # print("Number of nodes in the last tree is: {} with ccp_alpha: {}".format(
+    #     clfs[-1].tree_.node_count, ccp_alphas[-1]))
+
+    # clfs = clfs[:-1]
+    # ccp_alphas = ccp_alphas[:-1]
+
+    # node_counts = [clf.tree_.node_count for clf in clfs]
+    # depth = [clf.tree_.max_depth for clf in clfs]
+    # fig, ax = plt.subplots(2, 1)
+    # ax[0].plot(ccp_alphas, node_counts, marker='o', drawstyle="steps-post")
+    # ax[0].set_xlabel("alpha")
+    # ax[0].set_ylabel("number of nodes")
+    # ax[0].set_title("Number of nodes vs alpha")
+    # ax[1].plot(ccp_alphas, depth, marker='o', drawstyle="steps-post")
+    # ax[1].set_xlabel("alpha")
+    # ax[1].set_ylabel("depth of tree")
+    # ax[1].set_title("Depth vs alpha")
+    # fig.tight_layout()
+    # plt.savefig(get_base_dir('images')+'/Number of nodes vs alpha.png', dpi = 300)
+    # # plt.show()
+    # plt.close()
+
+    # train_scores = [clf.score(X_train, y_train) for clf in clfs]
+    # test_scores = [clf.score(X_test, y_test) for clf in clfs]
+
+    # fig, ax = plt.subplots()
+    # ax.set_xlabel("alpha")
+    # ax.set_ylabel("accuracy")
+    # ax.set_title("Accuracy vs alpha for training and testing sets")
+    # ax.plot(ccp_alphas, train_scores, marker='o', label="train",
+    #         drawstyle="steps-post")
+    # ax.plot(ccp_alphas, test_scores, marker='o', label="test",
+    #         drawstyle="steps-post")
+    # ax.legend()
+    # plt.savefig(get_base_dir('images')+'/Accuracy vs alpha.png', dpi = 300)
+    # plt.show()
+    # plt.close()
+    #  적정 ccp_alpha 값을 찾기 위한 소스 끝
+
+    tree_model.fit(X_train_std, y_train)
     dot_data = export_graphviz(tree_model
         , filled=True
         , rounded=True
@@ -69,11 +126,11 @@ if __name__ == '__main__':
 
     y_pred = tree_model.predict(X_test_std)
     print('잘못 분류된 샘플 개수: %d' % (y_test != y_pred).sum())
-    print("정확도 %.3f" % accuracy_score(y_test, y_pred))
+    # print("정확도 %.3f" % accuracy_score(y_test, y_pred))
     print("정확도 %.3f" % tree_model.score(X_test_std, y_test))
 
     target_names = ['Iris Setosa', 'Iris Versicolour', 'Iris Virginica']
-    print("confution matrix \n",multilabel_confusion_matrix(y_test, y_pred,labels=[0, 1, 2]))
+    print("confution matrix \n",multilabel_confusion_matrix(y_test, y_pred,labels=[2, 1, 0]))
     print(classification_report(y_test,y_pred, target_names=target_names))
 
     X_combined_std = np.vstack((X_train_std, X_test_std))
@@ -88,7 +145,7 @@ if __name__ == '__main__':
 
     plt.tight_layout()
     plt.savefig(get_base_dir('images')+'/iris_decision_regisons_decisionTree_gini.png', dpi = 300)
-    plt.show()
+    # plt.show()
     plt.close()
     
    
